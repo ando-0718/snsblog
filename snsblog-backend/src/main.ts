f6f13c67620651b418abfe0ad90e72bf9a105adb
kenshin <kenshin.ando@japan-systems.co.jp>
2025-07-28 22:11:41 +0900

    cookie機能の追加

--- snsblog-backend/src/main.ts
+++ snsblog-backend/src/main.ts
@@ -1,12 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
   const configService = app.get(ConfigService);
+  app.useGlobalPipes(new ValidationPipe());
+  app.use(cookieParser());
+  app.enableCors({
+    origin: configService.get<string>('FRONTEND_URL'), // Next.jsのURL
+    credentials: true,               // Cookie送信を許可
+  });
+  
   await app.listen(configService.get<number>('PORT') ?? 4000);
 }
 bootstrap();
